b4d82c2a04ecbca478db084057dec4b4de2782ca
Sander Weiss <weisssander@gmail.com>
2023-02-20 18:02:45 -0500

    added add employee feature

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(7,2) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -22,11 +22,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT NOT NULL
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
     password: '12621262',
-    database: ''
+    database: 'employee_db'
   },
-  );
-  db.query('', function (err, results) {
-  console.log(results);
-});
+);
 
 const questions = [
   {
@@ -22,6 +18,7 @@ const questions = [
     choices: [
       'View all departments',
       'View all employees',
+      'View all roles',
       'Add a department',
       'Add a role',
       'Add an employee',
@@ -30,27 +27,52 @@ const questions = [
   }
 ];
 
+const questionsTwo = [
+  {
+    type: 'input',
+    message: 'Input first name:',
+    name: 'first'
+  },
+  {
+    type: 'input',
+    message: 'Input last name:',
+    name: 'last'
+  }
+];
+
+const questionsThree = [
+  {
+    type: 'input',
+    message: 'Input new department name:',
+    name: 'department'
+  }
+];
+
 inquire()
 
 function inquire() {
   inquirer
-  .prompt(questions)
+    .prompt(questions)
     .then((answers) => {
-      switch (answers) {
+      console.log(answers)
+      switch (answers.start) {
         case 'View all departments':
-          console.log('View all departments')
+          viewDepartments()
           break;
         case 'View all employees':
-          console.log('View all employees')
+          viewEmployees()
+          break;
+        case 'View all roles':
+          viewRoles()
           break;
         case 'Add a department':
-          console.log('Add a department')
+          addDepartment()
           break;
         case 'Add a role':
           console.log('Add a role')
           break;
         case 'Add an employee':
-          console.log('Add an employee')
+          addEmployee()
           break;
         case 'Update an employee role':
           console.log('Update an employee role')
@@ -59,4 +81,83 @@ function inquire() {
     })
 }
 
+function viewDepartments() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results)
+  })
+}
 
+function viewEmployees() {
+  db.query('Select * FROM employee', function (err, results) {
+    console.table(results)
+  })
+}
+
+function viewRoles() {
+  db.query('Select * FROM roles', function (err, results) {
+    console.table(results)
+  })
+}
+
+function addDepartment() {
+  inquirer
+    .prompt(questionsThree)
+    .then(answer => {
+      
+    })
+}
+
+function addEmployee() {
+  inquirer
+    .prompt(questionsTwo)
+    .then(answers => {
+      let firstName = answers.first;
+      let lastName = answers.last;
+      db.promise().query('SELECT * FROM roles').then(response => {
+        // console.log(response[0])
+        let roleChoices = response[0].map(({ id, title }) => ({
+          name: title,
+          value: id
+        }))
+        console.log(roleChoices)
+        inquirer
+          .prompt({
+            type: 'list',
+            message: 'What is employees role?',
+            name: 'roleid',
+            choices: roleChoices
+          })
+          .then(response => {
+            let roleid = response.roleid
+            db.promise().query('SELECT * FROM employee').then(response => {
+              let managerChoices = response[0].map(({ id, first_name, last_name }) => ({
+                name: `${first_name} ${last_name}`,
+                value: id
+              }))
+              console.log(managerChoices)
+              managerChoices.unshift({name: 'none', value: null})
+              inquirer
+              .prompt({
+                type: 'list',
+                message: 'What is employees manager?',
+                name: 'managerid',
+                choices: managerChoices
+              })
+              .then(response => {
+                let employee = {
+                  first_name: firstName,
+                  last_name: lastName,
+                  role_id: roleid,
+                  manager_id: response.managerid
+                }
+                console.log(employee)
+                db.promise().query('INSERT INTO employee SET ?', employee)
+                .then(() => {
+                  console.log('successfully added employee')
+                })
+              })
+            })
+          })
+      })
+    })
+}
